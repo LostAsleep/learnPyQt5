621d78e0e27c83f2096099a501b0b325c0dc757c
LostAsleep <phammersen@protonmail.com>
2021-11-20 21:14:38 +0100

    Going on...

---
+++ main00.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import sys  # Only needed for access to command line arguments.
+
+
+# You need on (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works. too.
+app = QApplication(sys.argv)
+
+window = QMainWindow()
+window.show()  # IMPORTANT! Windows are hidden by default
+
+# Start the event loop.
+app.exec_()
+
+
+# Your application won't reach here until you exit and the
+# event loop has stopped.
+
