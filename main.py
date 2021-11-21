9c122fe2b5f77b561f5fe60f7432178ac637cfc3
LostAsleep <phammersen@protonmail.com>
2021-11-21 11:17:50 +0100

    Add buttons and a check box to the toolbar.

--- main.py
+++ main.py
@@ -21,16 +21,28 @@ class MainWindow(QMainWindow):
         toolbar = QToolBar("The main toolbar")
         # Prevent the toolbar from being removed via right-click menu
         toolbar.setContextMenuPolicy(Qt.PreventContextMenu)
+        toolbar.setIconSize(QSize(16, 16))  # Set icon size to prevent lots of padding
         self.addToolBar(toolbar)
         
         # You must also pass in any QObject to act as the parent for the action
         # here we’re passing self as a reference to our main window. Strangely
         # for QAction the parent element is passed in as the final parameter.
-        button_action = QAction("Your button", self)
+        button_action = QAction(QIcon("./bug.png"), "Your button", self)
         button_action.setStatusTip("This is your button")  # For the status bar
         button_action.triggered.connect(self.onMyToolBarButtonClick)
         button_action.setCheckable(True)
         toolbar.addAction(button_action)
+
+        toolbar.addSeparator()
+
+        button_action2 = QAction(QIcon("./bug.png"), "Your button2", self)
+        button_action2.setStatusTip("This is your button2")
+        button_action2.triggered.connect(self.onMyToolBarButtonClick)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)
+
+        toolbar.addWidget(QLabel("Hello"))
+        toolbar.addWidget(QCheckBox())
 
         self.setStatusBar(QStatusBar(self))
 
